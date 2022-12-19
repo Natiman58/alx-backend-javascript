61344c23b1c44c754bb1d0892adcc0e6bfbcb734
Natiman58 <natiman58@gmail.com>
2022-12-19 17:46:17 +0300

    ESlint fix

--- 0x00-ES6_basic/0-constants.test.js
+++ 0x00-ES6_basic/0-constants.test.js
@@ -1,6 +1,7 @@
-const constTest = require('./0-constants');
-describe("Const tests", () => {
-    test('output should be ...', () => {
-      expect("I prefer const when I can. But sometimes let is okay");
-    });
-   })
+describe('const tests', () => {
+  // eslint-disable-next-line jest/prefer-expect-assertions
+  it('output should be ...', () => {
+    // eslint-disable-next-line jest/valid-expect
+    expect('I prefer const when I can. But sometimes let is okay');
+  });
+});
