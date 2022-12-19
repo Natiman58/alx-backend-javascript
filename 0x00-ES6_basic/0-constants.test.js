8d3954e7da8a773e837c0b6268ee24d26116bf48
Natiman58 <natiman58@gmail.com>
2022-12-19 17:40:33 +0300

    test pas

---
+++ 0x00-ES6_basic/0-constants.test.js
@@ -0,0 +1,6 @@
+const constTest = require('./0-constants');
+describe("Const tests", () => {
+    test('output should be ...', () => {
+      expect("I prefer const when I can. But sometimes let is okay");
+    });
+   })
