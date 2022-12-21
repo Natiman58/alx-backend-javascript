03713dedd7556400dfb9ef2bb313e6a056a51a9f
Natiman58 <natiman58@gmail.com>
2022-12-21 16:48:42 +0300

    console log remove

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,5 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then(
-    (result) => console.log(result),
-  );
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
 }
