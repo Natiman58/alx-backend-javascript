647ad02590585d0258b1398eca9480532494dc34
Natiman58 <natiman58@gmail.com>
2022-12-21 17:05:56 +0300

    fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,9 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((result) => result.map((key) => ({
+      status: key.status,
+      value: key.status === 'fulfilled' ? key.value : String(key.reason),
+    })));
 }
