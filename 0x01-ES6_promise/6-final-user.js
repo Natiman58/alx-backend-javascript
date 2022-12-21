4f5af8b051925734883ef234e1c37b90b68fb43c
Natiman58 <natiman58@gmail.com>
2022-12-21 16:39:43 +0300

    status

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,8 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise
-    .all([signUpUser, uploadPhoto]).then(['{ status: status_of_the_promise, value: value or error returned by the Promise}']);
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then(
+    (result) => console.log(result),
+  );
 }
