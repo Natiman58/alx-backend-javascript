b3f8768c670706e6d4c577a5f3f00f488f50e684
Natiman58 <natiman58@gmail.com>
2022-12-21 12:40:13 +0300

    resolve

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,4 @@
+export default function signUpUser(firstName, lastName) {
+  // eslint-disable-next-line no-unused-expressions, no-sequences, new-cap
+  return Promise.resolve({ firstName, lastName });
+}
