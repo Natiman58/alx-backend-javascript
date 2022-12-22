fcb4228ab7f2348b5044789b2d4b7a83c2ef3a94
Natiman58 <natiman58@gmail.com>
2022-12-22 18:44:37 +0300

    imported class

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,36 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getters
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  // Setters
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  // Method that returns attributes in the the format <amount> <currency_name>
+  displayFullPrice() {
+    return (`${this.amount} ${this.currency.name} (${this.currency.code})`);
+  }
+
+  // static method
+  static convertPrice(amount, conversionRate) {
+    return (amount * conversionRate);
+  }
+}
