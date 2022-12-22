84dc1992c9b9ce1b41516b735a70fd846f8f2a37
Natiman58 <natiman58@gmail.com>
2022-12-22 23:48:52 +0300

    species

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,17 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  // Method thar returns the new object of the class
+  // eslint-disable-next-line class-methods-use-this
+  cloneCar() {
+    return new this.constructor[Symbol.species]();
+  }
+}
