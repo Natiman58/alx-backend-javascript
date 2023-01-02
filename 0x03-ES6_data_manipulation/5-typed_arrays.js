080794c8028573493b37812c8452c63b15ac1c49
Natiman58 <natiman58@gmail.com>
2023-01-02 21:29:16 +0300

    create 8 bit integer array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,13 @@
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length, position, value);
+  const buffer = new ArrayBuffer(length);
   const dataView = new DataView(buffer);
+
+  // throw error if the byteOffset position in >= length of the buffer
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  // else store 8 bit integer value in the specified byte array position
+  dataView.setInt8(position, value);
   return dataView;
 }
