37302a1c194a8ecb7914b6f225e1a32d3d50241a
Natiman58 <natiman58@gmail.com>
2023-01-02 22:16:49 +0300

    every value in set

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,11 +1,3 @@
 export default function hasValuesFromArray(set, array) {
-  let containsArray = false;
-  const checkForArr = array;
-  for (const array of set) {
-    if (array.toString() === checkForArr.toString()) {
-      containsArray = true;
-      break;
-    }
-  }
-  return console.log(containsArray);
+  return array.every((value) => set.has(value));
 }
