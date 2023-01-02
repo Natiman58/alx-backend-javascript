c30721be4f99d8e511fd44ab51ecdb2c3eb9e8f1
Natiman58 <natiman58@gmail.com>
2023-01-02 18:21:22 +0300

    spell

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,7 +2,7 @@ export default function getListStudents() {
   const arrayObj = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Fransicso' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 
   return arrayObj;
