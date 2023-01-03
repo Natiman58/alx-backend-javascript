d6c6ab99b5a577d16ed0713d92291b7c09b9a56b
Natiman58 <natiman58@gmail.com>
2023-01-03 13:39:41 +0300

    entries

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,12 +1,12 @@
 export default function updateUniqueItems(mapObj) {
-  if (!(mapObj instanceof Map)) {
-    throw new Error('Cannot Process');
-  }
-
-  for (const [key, val] of mapObj.entries()) {
-    if (val === 1) {
-      mapObj.set(key, 100);
+  if (mapObj instanceof Map) {
+    for (const [key, val] of mapObj.entries()) {
+      if (val === 1) {
+        mapObj.set(key, 100);
+      }
     }
+  } else {
+    throw new Error('Cannot Process');
   }
   return mapObj;
 }
