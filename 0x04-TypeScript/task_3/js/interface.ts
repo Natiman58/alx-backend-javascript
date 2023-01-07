ec3ce36cf94e53724c2c35c00d933c20f36c699e
Natiman58 <natiman58@gmail.com>
2023-01-07 22:14:07 +0300

    task_3 done

---
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -0,0 +1,10 @@
+
+// 1. create a type RowID and set it number
+export type RowID = number
+
+// create an interface RowElement that contains 3 fields
+export interface RowElement {
+    firstName: string;
+    lastName: string;
+    age?: number;
+}
---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,24 @@
+/// <reference path="./crud.d.ts" />
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud';
+// create an obj with type RowElement
+// with fields firstName, lastName
+const row: RowElement = {
+    firstName: 'Guillaume',
+    lastName: 'Salva',
+}
+
+
+// create a const var named row with type RowID
+// and assign the insertRow command from CRUD
+const newRowID: RowID = CRUD.insertRow(row);
+
+// create a const variable named updateRow with type RowElement
+// then update row with an age field set to 23
+const updateRow: RowElement = {
+    ...row, age: 23
+}
+
+// Finally call the methods updateRow and deleteRow
+console.log(CRUD.updateRow(newRowID, updateRow));
+console.log(CRUD.deleteRow(newRowID));
