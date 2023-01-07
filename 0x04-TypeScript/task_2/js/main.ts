6b378070149f8ec96e192bf02da68deb4291c7b6
Natiman58 <natiman58@gmail.com>
2023-01-07 19:34:16 +0300

    task_2 complete

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,6 +1,6 @@
 
 interface DirectorInterface {
-    // methods
+    // methods return strings
     workFromHome(): string;
     getCoffeeBreak(): string;
     workDirectorTasks(): string;
@@ -39,13 +39,54 @@ class Teacher implements TeacherInterface {
     workTeacherTasks(): string {
         return 'Getting to work';
     }
+
 }
 
+// A function that returns a Teacher or Director instance and accepts one arg 'salary'
 function createEmployee(salary: string | number): Teacher | Director {
     if (typeof salary === 'number' && salary < 500) {
-        return new Teacher;
+        return new Teacher();
     };
-    return new Director;
+    return new Director();
 
 }
-console.log(createEmployee(1200));
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+
+
+
+// A booloean function that accepts employee class as an argument
+// use employee as a predicate
+// and returns the instance of the emplyoee as Director
+function isDirector(employee: (Director | Teacher)): employee is Director {
+
+    return (employee instanceof Director);
+}
+
+
+// A function that accepts employee as argument
+// if employee is Director call workDirectorTasks class method
+// else call workTeacherTasks class method
+function executeWork(employee: (Director | Teacher)) {
+    if (isDirector(employee)) {
+        return (employee as Director).workDirectorTasks();
+    }
+    return (employee as Teacher).workTeacherTasks();
+}
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)))
+
+// A tring literal type named Subjects that can be Math or History
+type Subjects = ( 'Math' | 'History' );
+
+// A function named teachClass
+function teachClass(todayClass: Subjects): string {
+    if (todayClass === 'Math') {
+        return 'Teaching Math';
+    }
+    return 'Teaching History'
+}
+
+console.log(teachClass('Math'))
+console.log(teachClass('History'))
