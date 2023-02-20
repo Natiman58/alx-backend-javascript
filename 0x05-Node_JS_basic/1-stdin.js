ff9f699c18c1c1e0a817da13fe68b28beea08042
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-20 18:49:12 +0300

    outside

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,13 @@
 // a simple script that uses process stdin
+process.stdin.setEncoding('utf-8');
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
+  process.exit();
+});
 
-  process.on('exit', () => {
-    console.log('This important software is now closing');
-  });
+process.on('exit', () => {
+  console.log('This important software is now closing');
   process.exit();
 });
