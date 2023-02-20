eba0b393ce640b3f9e2b6410d98496caa7f30ca6
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-20 18:23:12 +0300

    remove new line

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
 // a simple script that uses process stdin
-console.log('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
 
   process.on('exit', () => {
-    console.log('This important software is now closing\n');
+    console.log('This important software is now closing');
   });
   process.exit();
 });
