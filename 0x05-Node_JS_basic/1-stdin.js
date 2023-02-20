a777d38f614e860caa10e5bd9118d0a590bd2605
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-20 20:31:07 +0300

    try readable fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,20 +1,15 @@
 // a simple script that uses process stdin
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// if the process is connected to terminal(isTTY)
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    process.stdout.write(`Your name is: ${name}`);
-    process.exit();
-  });
-} else {
-  process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    process.stdout.write(`Your name is: ${name}`);
-    process.exit();
-  });
-  process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
-}
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+// if the data is readable
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+// in the end of the read file
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
