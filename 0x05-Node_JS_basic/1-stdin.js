cb384bdde3fb98caf032b6b69dca07c4a845823e
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-20 19:33:44 +0300

    exit first

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -12,6 +12,7 @@ if (process.stdin.isTTY) {
   process.stdin.on('data', (data) => {
     const name = data.toString().trim();
     console.log(`Your name is: ${name}`);
+    process.exit();
   });
   process.on('exit', () => {
     console.log('This important software is now closing');
