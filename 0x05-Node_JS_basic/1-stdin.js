576781646558decfa79cd6d5eefb7647b758d074
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-20 19:28:15 +0300

    mange the diffrent streams

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,20 @@
 // a simple script that uses process stdin
 process.stdin.setEncoding('utf-8');
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
 
-  process.on('exit', () => {
-    console.log('This important software is now closing');
+// if the process is connected to terminal
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    const name = data.toString().trim();
+    console.log(`Your name is: ${name}`);
+    process.exit();
   });
-  process.exit();
-});
+} else {
+  process.stdin.on('data', (data) => {
+    const name = data.toString().trim();
+    console.log(`Your name is: ${name}`);
+    process.on('exit', () => {
+      console.log('This important software is now closing');
+    });
+  });
+}
