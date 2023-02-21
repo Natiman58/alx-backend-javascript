7839f2c54547f4c8dca8e8117dab01cf66498c62
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-21 20:36:12 +0300

    some fix

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,56 +1,56 @@
-// A script that creates a more complex HTTP server using Node JS
-
-// import the http, url modules
+// import the http and url modules
 const http = require('http');
 const url = require('url');
 
-// Import the getStudents and getStudentsFromDB functions from the 3-read_file_async.js file
-const { countStudents } = require('./3-read_file_async');
+// Import the countStudents function from the 3-read_file_async.js file
+const countStudents = require('./3-read_file_async');
 
 // set the host and port number
 const host = 'localhost';
 const port = 1245;
 
 // A function to handle incoming HTTP requests and return HTTP response
-// req obj: captures all the incoming http requests
-// res obj: returns the http response for the server
-const requestListner = (req, res) => {
+const requestListener = (req, res) => {
   // parse the requested url
   const parsedUrl = url.parse(req.url, true);
+
   // extract the path from the parsed url
   const { pathname } = parsedUrl;
 
-  // if the path is ('/') -> send 'Hello Hol...' response
+  // if the path is ('/') -> send 'Hello Holberton School!' response
   if (pathname === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
 
-    // if the path is ('/students') -> call the db
+    // if the path is ('/students') -> call the countStudents function
   } else if (pathname === '/students') {
-    const { db } = parsedUrl.query;
+    const db = parsedUrl.query;
+
+    // if db is provided, call the countStudents function with the specified db
     if (db) {
       countStudents(db, (error, data) => {
         if (error) {
-          // send 500 response
-          res.writeHead(500, { 'Content-Type': 'text/plain' });
-          res.end('Internal server error');
-        } else {
-          // if no error
-          res.writeHead(200, { 'Content-Type': 'text/plain' });
-          res.end(data);
-        }
-      });
-    } else {
-      // if no db call the getStudents function
-      countStudents((error, data) => {
-        if (error) {
-          // if there is error -> send 500 response code
+          // if there is an error -> send 500 response
           res.writeHead(500, { 'Content-Type': 'text/plain' });
           res.end('Internal server error');
         } else {
           // if no error -> 200 response
           res.writeHead(200, { 'Content-Type': 'text/plain' });
-          res.end(data);
+          res.end(`Number of students: ${data.count}. List: ${data.students.join(', ')}`);
+        }
+      });
+
+      // if db is not provided, call the countStudents function with no arguments
+    } else {
+      countStudents((error, data) => {
+        if (error) {
+          // if there is an error -> send 500 response
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('Internal server error');
+        } else {
+          // if no error -> 200 response
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          res.end(`Number of students: ${data.count}. List: ${data.students.join(', ')}`);
         }
       });
     }
@@ -62,11 +62,11 @@ const requestListner = (req, res) => {
   }
 };
 
-// then create the server
-const app = http.createServer(requestListner);
-// bind it to network address
-app.listen(port, host, () => {
+// create the server and bind it to the network address
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
   console.log(`Server is running on http://${host}:${port}`);
 });
 
-module.exports = app;
+// export the server for testing purposes
+module.exports = server;
