ca7940d9858aacaf64ed3080b34155fdf3039c8d
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-21 17:09:34 +0300

    export the server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -10,14 +10,16 @@ const port = 1245;
 // A function to handle incoming HTTP requests and return HTTP response
 // req obj: captures all the incoming http requests
 // res obj: returns the http response for the server
-const app = (req, res) => {
-  res.writeHead(200);
+const requestListner = (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
 };
 
 // then create the server
-const server = http.createServer(app);
+const app = http.createServer(requestListner);
 // bind it to network address
-server.listen(port, host, () => {
+app.listen(port, host, () => {
   console.log(`Server is running on http://${host}:${port}`);
 });
+
+module.exports = app;
