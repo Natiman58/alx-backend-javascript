b99223fe6c0ac863dc148e0eac1dfe148ff3a773
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-23 16:18:05 +0300

    destructure and import

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,10 +1,11 @@
 const assert = require('assert');
-const calculate = require('./0-calcul.js');
+const { calculateNumber } = require('./0-calcul.js');
 
 describe('calculate', function() {
-    it('should return the sum of the two rounded numbers', function() {
-        assert.equal(calculate.calculateNumber(3.4, 4.6), 8);
-        assert.equal(calculate.calculateNumber(3, 4.6), 8);
-        assert.equal(calculate.calculateNumber(-1, 3.3), 2);
+    it('should return the sum of the two rounded numbers.', function() {
+        assert.equal(calculateNumber(1, 3), 4);
+        assert.equal(calculateNumber(1, 3.7), 5);
+        assert.equal(calculateNumber(1.2, 3.7), 5);
+        assert.equal(calculateNumber(1.5, 3.7), 6);
     });
 });
