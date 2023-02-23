779a8e82932fa896fe58e0a9a5df1c66cce7fed4
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-23 22:19:20 +0300

    dec

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -3,5 +3,5 @@ function calculateNumber(a, b) {
     return Math.round(a) + Math.round(b)
 };
 
-module.exports.calculateNumber = calculateNumber;
+module.exports.calculate = calculateNumber;
 //module.exports = calculateNumber;
