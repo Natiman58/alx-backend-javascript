3582074c1826cd041b628742f81e922fbb45e911
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-24 00:29:08 +0300

    chai 2

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -2,4 +2,7 @@ const expect = require('chai').expect; // Using Expect style
 const calculateNumber = require('./2-calcul_chai'); // 6✅
 
 
-expect(calculateNumber("SUM", 1, 2)).to.be.equal(3)
+expect(calculateNumber("SUM", 1.4, 4.5)).to.be.equal(6)
+expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.be.equal(-4)
+expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.be.equal(0.2)
+expect(calculateNumber('DIVIDE', 1.4, 0)).to.be.equal('Error')
