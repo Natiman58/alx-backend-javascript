833eda302660d0bc798fb5c96c42aa71d32960ad
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-23 22:56:21 +0300

    fix

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,7 +5,7 @@ describe('calculateNumber', function() {
     it('should return the sum of the two rounded numbers.', function() {
         assert.equal(calculate(1, 3), 4);
         assert.equal(calculate(1, 3.7), 5);
-        assert.equal(calculate(1.2, 3.7), 5);
+        assert.equal(calculate(1.2, 3.7), 6);
         //assert.equal(calculateNumber(1.5, 3.7), 6);
         //assert.equal(calculateNumber(1000e122222, 3.7), Infinity);
     });
