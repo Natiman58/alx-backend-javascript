ea9a32c8ffb0e4edc1fe2b5800653acd3ff7d80a
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-24 23:10:56 +0300

    more tests

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -8,4 +8,12 @@ describe('Index page', () => {
         const text = await response.text()
         assert.strictEqual(text, 'Welcome to the payment system');
     });
+    it('should return the status code 200', async() => {
+        const response = await fetch('http://localhost:7865/');
+        assert.strictEqual(response.status, 200);
+    })
+    it('should return the status code 404 when hitting invalid endpoint', async() => {
+        const response = await fetch('http://localhost:7865/invalid');
+        assert.strictEqual(response.status, 404);
+    })
 });
