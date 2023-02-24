c223da9c536451448a78c85a4f153ab3f4f68796
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-25 00:34:48 +0300

    index

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,19 +1,33 @@
 const assert = require('assert');
 const fetch = require('node-fetch');
+const app = require('./api');
 
 describe('Index page', () => {
+    let server;
+
+    beforeEach(async() => {
+        server = app.listen(7866);
+    });
+
+    afterEach(async() => {
+        server.close(() => {
+            console.log('Server closed');
+        });
+    });
+
     it('should return a welcome message', async() => {
-        const response = await fetch('http://localhost:7865/');
-        // uset text() method to get the body of the response
-        const text = await response.text()
+        const response = await fetch('http://localhost:7866/');
+        const text = await response.text();
         assert.strictEqual(text, 'Welcome to the payment system');
     });
+
     it('should return the status code 200', async() => {
-        const response = await fetch('http://localhost:7865/');
+        const response = await fetch('http://localhost:7866/');
         assert.strictEqual(response.status, 200);
-    })
+    });
+
     it('should return the status code 404 when hitting invalid endpoint', async() => {
-        const response = await fetch('http://localhost:7865/invalid');
+        const response = await fetch('http://localhost:7866/invalid');
         assert.strictEqual(response.status, 404);
-    })
+    });
 });
