5774746aa2ec77a0b45ecc2406a127f7a5e73547
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-24 21:35:18 +0300

    async

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,17 +1,15 @@
-// test suite using done
+// test suite without using done
 const assert = require('assert');
 const getPaymentTokenFromAPI = require('./6-payment_token')
 
 
 describe("getPaymentTokenFromApI", function() {
-    it("sould return a resolved promise obj with a data when success is true", function() {
-        return getPaymentTokenFromAPI(true).then(function(response) {
-            assert.deepEqual(response, { data: 'Successful response from the API' });
-        });
+    it("sould return a resolved promise obj with a data when success is true", async function() {
+        const response = await getPaymentTokenFromAPI(true);
+        assert.deepEqual(response, { data: 'Successful response from the API' });
     });
-    it("sould return a resolved promise obj with no data when success is false", function() {
-        return getPaymentTokenFromAPI(false).then(function(response) {
-            assert.strictEqual(response, undefined);
-        });
+    it("sould return a resolved promise obj with no data when success is false", async function() {
+        const response = await getPaymentTokenFromAPI(false);
+        assert.strictEqual(response, undefined);
     });
 });
