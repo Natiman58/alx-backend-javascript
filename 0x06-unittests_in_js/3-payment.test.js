c587fff93eb9f2ecfe1340913ae2d93105bb76c9
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-24 12:07:17 +0300

    more comments

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -7,9 +7,12 @@ const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', function() {
     it('should call Utils.calculateNumber with "SUM", 100, 20', function() {
+        // spy on Utils.cal... function
         const spy = sinon.spy(Utils, 'calculateNumber');
         sendPaymentRequestToApi(100, 20);
         assert(spy.calledOnceWithExactly('SUM', 100, 20));
+        // To restore the orignal implementation of Utils.cal...
+        // to prevent any interference with other tests
         spy.restore();
     });
 });
