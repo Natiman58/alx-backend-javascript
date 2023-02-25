3612d3879a0f18d7a6d0f0b69957c78cf47218c9
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-25 13:03:50 +0300

    make request function

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,21 +1,45 @@
 const assert = require('assert');
 const fetch = require('node-fetch');
+const expect = require('chai').expect;
 //const app = require('./api');
 
+async function makeRequest(options) {
+    const response = await fetch(options.url, { method: options.method });
+    //console.log(response);
+    const body = await response.text();
+    const { headers, status } = response;
+    return { statusCode: status, body, headers };
+}
+
+
 describe('Index page', () => {
+    const options = {
+        url: "http://localhost:7865/",
+        method: "GET"
+    };
 
     it('should return a welcome message', async() => {
-        const response = await fetch('http://localhost:7865/');
-        const text = await response.text();
-        assert.strictEqual(text, 'Welcome to the payment system');
+        const { body } = await makeRequest(options);
+        expect(body).to.contain("Welcome to the payment system");
+        //const response = await fetch('http://localhost:7865/');
+        //const text = await response.text();
+        //assert.strictEqual(text, 'Welcome to the payment system');
     });
 
     it('should return the status code 200', async() => {
-        const response = await fetch('http://localhost:7865/');
-        assert.strictEqual(response.status, 200);
+        const { statusCode } = await makeRequest(options);
+        expect(statusCode).to.be.equal(200);
+
+        //const response = await fetch('http://localhost:7865/');
+        //assert.strictEqual(response.status, 200);
     });
 
-    it('should return the status code 404 when hitting invalid endpoint', async() => {
+    it('should check corrent content length', async() => {
+        const { headers } = await makeRequest(options);
+        expect(headers.get('content-length')).to.equal('29');
+    })
+
+    it.skip('should return the status code 404 when hitting invalid endpoint', async() => {
         const response = await fetch('http://localhost:7865/invalid');
         assert.strictEqual(response.status, 404);
     });
