d37d7c22371da2a233b1b948f8b8ec292e637996
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-25 13:49:21 +0300

    useing done

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,46 +1,29 @@
-const assert = require('assert');
+const request = require('request');
 const fetch = require('node-fetch');
 const expect = require('chai').expect;
 //const app = require('./api');
-
-async function makeRequest(options) {
-    const response = await fetch(options.url, { method: options.method });
-    //console.log(response);
-    const body = await response.text();
-    const { headers, status } = response;
-    return { statusCode: status, body, headers };
-}
-
+const url = "http://localhost:7865";
 
 describe('Index page', () => {
-    const options = {
-        url: "http://localhost:7865/",
-        method: "GET"
-    };
-
-    it('should return a welcome message', async() => {
-        const { body } = await makeRequest(options);
-        expect(body).to.contain("Welcome to the payment system");
-        //const response = await fetch('http://localhost:7865/');
-        //const text = await response.text();
-        //assert.strictEqual(text, 'Welcome to the payment system');
+    it('should return the status code 200', (done) => {
+        request.get(url, (error, response) => {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
     });
 
-    it('should return the status code 200', async() => {
-        const { statusCode } = await makeRequest(options);
-        expect(statusCode).to.be.equal(200);
-
-        //const response = await fetch('http://localhost:7865/');
-        //assert.strictEqual(response.status, 200);
-    });
-
-    it('should check corrent content length', async() => {
-        const { headers } = await makeRequest(options);
-        expect(headers.get('content-length')).to.equal('29');
+    it('should return Welcome to the payment system', (done) => {
+        request.get(url, (error, response) => {
+            expect(response.body).to.equal('Welcome to the payment system');
+            done();
+        })
     })
 
-    it.skip('should return the status code 404 when hitting invalid endpoint', async() => {
-        const response = await fetch('http://localhost:7865/invalid');
-        assert.strictEqual(response.status, 404);
-    });
-});
+    it('should return the correct content lenght', (done) => {
+        request.get(url, (error, response) => {
+            expect(response.headers['content-length']).to.equal('29')
+            done();
+        })
+    })
+
+});;
