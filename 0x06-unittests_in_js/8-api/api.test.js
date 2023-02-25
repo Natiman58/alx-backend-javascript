359cd60d1f148a8063653cbdef68433d60912bc2
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-25 14:03:18 +0300

    add few comments

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,11 +1,13 @@
-const request = require('request');
-const expect = require('chai').expect;
+const request = require('request'); // import requests for HTTP request
+const expect = require('chai').expect; // import expect from chai assertion lib
 
+// define the test suite for the index page
 describe('Index page', () => {
-    const options = {
-        url: "http://localhost:7865/",
-        method: "GET"
-    }
+    const options = { // set options for the HTTP request to the index page
+            url: "http://localhost:7865/",
+            method: "GET"
+        }
+        // Test the it returns the correct status code
     it('should return the status code 200', (done) => {
         request.get(options, (error, response) => {
             expect(response.statusCode).to.equal(200);
@@ -13,6 +15,7 @@ describe('Index page', () => {
         });
     });
 
+    // Test it returns the correct message
     it('should return Welcome to the payment system', (done) => {
         request.get(options, (error, response) => {
             expect(response.body).to.equal('Welcome to the payment system');
@@ -20,6 +23,7 @@ describe('Index page', () => {
         })
     })
 
+    // Test it the correct content length
     it('should return the correct content lenght', (done) => {
         request.get(options, (error, response) => {
             expect(response.headers['content-length']).to.equal('29')
