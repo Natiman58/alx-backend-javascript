a12cf692c45b04b5b89d15e49208cee1ddb0a163
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-25 13:52:29 +0300

    add GET method

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,25 +2,28 @@ const request = require('request');
 const fetch = require('node-fetch');
 const expect = require('chai').expect;
 //const app = require('./api');
-const url = "http://localhost:7865";
 
 describe('Index page', () => {
+    const options = {
+        url: "http://localhost:7865/",
+        method: "GET"
+    }
     it('should return the status code 200', (done) => {
-        request.get(url, (error, response) => {
+        request.get(options, (error, response) => {
             expect(response.statusCode).to.equal(200);
             done();
         });
     });
 
     it('should return Welcome to the payment system', (done) => {
-        request.get(url, (error, response) => {
+        request.get(options, (error, response) => {
             expect(response.body).to.equal('Welcome to the payment system');
             done();
         })
     })
 
     it('should return the correct content lenght', (done) => {
-        request.get(url, (error, response) => {
+        request.get(options, (error, response) => {
             expect(response.headers['content-length']).to.equal('29')
             done();
         })
