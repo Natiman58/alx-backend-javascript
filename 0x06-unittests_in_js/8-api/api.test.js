9feed1c102883f34745962a29838c744d5d8b482
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-25 12:23:41 +0300

    port fix

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,33 +1,22 @@
 const assert = require('assert');
 const fetch = require('node-fetch');
-const app = require('./api');
+//const app = require('./api');
 
 describe('Index page', () => {
-    let server;
-
-    beforeEach(async() => {
-        server = app.listen(7866);
-    });
-
-    afterEach(async() => {
-        server.close(() => {
-            console.log('Server closed');
-        });
-    });
 
     it('should return a welcome message', async() => {
-        const response = await fetch('http://localhost:7866/');
+        const response = await fetch('http://localhost:7865/');
         const text = await response.text();
         assert.strictEqual(text, 'Welcome to the payment system');
     });
 
     it('should return the status code 200', async() => {
-        const response = await fetch('http://localhost:7866/');
+        const response = await fetch('http://localhost:7865/');
         assert.strictEqual(response.status, 200);
     });
 
     it('should return the status code 404 when hitting invalid endpoint', async() => {
-        const response = await fetch('http://localhost:7866/invalid');
+        const response = await fetch('http://localhost:7865/invalid');
         assert.strictEqual(response.status, 404);
     });
 });
